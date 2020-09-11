b5e07470cc4bff2f9826bfe209969ac4259f3519
coralkoala <cor4lkoala@gmail.com>
2020-09-11 23:59:35 +0900

    solved 1008, 10171, 10172, 10430, 10869

---
+++ Mathematics/2588.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 0, b = 0;
+    cin >> a >> b;
+
+    cout << a * (b % 10) << endl;
+    cout << a * ((b / 10) % 10) << endl;
+    cout << a * (b / 100) << endl;
+    cout << a * b << endl;
+    return 0;
+}
